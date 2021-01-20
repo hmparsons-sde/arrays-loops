40291e6febcf24ef946fa21737cd2d16f16f556e
Holly Parsons <holly-parsons@outlook.com>
2021-01-19 20:41:11 -0600

    crazy

--- main.js
+++ main.js
@@ -11,8 +11,8 @@
 
 // ****ARRAYS WITH LOOPS
 
-const ec14Instructors = ['Aja', 'Trinity', 'Dr. T', 'Discussion Tickets'];
+// const ec14Instructors = ['Aja', 'Trinity', 'Dr. T', 'Discussion Tickets'];
 
-for (let i = 0; i < ec14Instructors.length; i++) {
-  console.log(ec14Instructors[i]);
-}
+// for (let i = 0; i < ec14Instructors.length; i++) {
+//   console.log(ec14Instructors[i]);
+//
